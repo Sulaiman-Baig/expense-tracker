87a67b3700aec617d0c8ad7ecef6aeba6c731640
sulaimanbaig <engsulaimanbaig@gmailcom>
2020-06-20 09:21:14 +0500

    Balance Component Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 
 import './App.css';
 
@@ -8,7 +9,9 @@ function App() {
   return (
     <div >
       <Header />
-
+      <div className='container'>
+        <Balance />
+      </div>
     </div>
   );
 }
