fcb70ab123d14a816eaae070087c21df193b78bc
sulaimanbaig <engsulaimanbaig@gmailcom>
2020-06-20 09:50:28 +0500

    AddTransaction Component Added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 import './App.css';
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
