89f6b82eaf808df91da64ef5e98701d1924a4f1e
sulaimanbaig <engsulaimanbaig@gmailcom>
2020-06-20 08:56:02 +0500

    Basic Project Setup
    Basic project setup + extra files are removed + css added + header component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Header } from './components/Header';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Expense Tracker</h1>
-      <h4>Developed by Sulaiman</h4>
+    <div >
+      <Header />
+
     </div>
   );
 }
